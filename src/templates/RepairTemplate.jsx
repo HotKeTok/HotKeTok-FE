c6f467deee1207f0eba23e522101c6d29b339ced
sunwoo-h <flexible8011@gmail.com>
2025-09-03 21:27:42 +0900

    feat: TextField 컴포넌트 구현

--- src/templates/RepairTemplate.jsx
+++ src/templates/RepairTemplate.jsx
@@ -1,12 +1,14 @@
+import TextField from '../components/common/TextField';
 import RepairBanner from '../components/Repair/RepairBanner';
 
 export default function RepairTemplate() {
   return (
-    <div>
+    <>
       <h1>뚝딱뚜깍</h1>
       <div style={{ padding: '15px' }}>
         <RepairBanner />
       </div>
-    </div>
+      <TextField placeholder="입력하시요" state="error" />
+    </>
   );
 }
