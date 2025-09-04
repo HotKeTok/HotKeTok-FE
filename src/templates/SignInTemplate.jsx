edb94019fb38a80718418834253dd31cda5ea055
sunwoo-h <flexible8011@gmail.com>
2025-09-04 20:34:12 +0900

    feat: SignIn 페이지 로고, 타이틀 퍼블리싱

--- src/templates/SignInTemplate.jsx
+++ src/templates/SignInTemplate.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import Logo from '../assets/common/BrandLogo.png';
+import styled from 'styled-components';
+import { color, typo } from '../styles/tokens';
+import { Column, FlexItem, Row } from '../styles/flex';
 
 export default function SignInTemplate() {
-  return <div>로그인페이지</div>;
+  return (
+    <>
+      <div style={{ height: '144px' }} />
+      <Column $gap={18} $align="center">
+        <LogoImg src={Logo} alt="핫케톡로고" />
+        <Title>로그인</Title>
+      </Column>
+    </>
+  );
 }
+
+const LogoImg = styled.img`
+  width: 14.5%;
+`;
+
+const Title = styled.div`
+  ${typo('h2')};
+  color: ${color('grayscale.700')};
+`;
