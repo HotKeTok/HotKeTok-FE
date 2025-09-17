747fa5365a74a477bfad6115bd5a0b4f42c01f04
sunwoo-h <flexible8011@gmail.com>
2025-09-17 23:20:44 +0900

    chore: 코드 누락건 해결

--- src/templates/main/NoticeTemplate.jsx
+++ src/templates/main/NoticeTemplate.jsx
@@ -1,4 +1,4 @@
-import React, from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import TopBar from '../../components/common/TopBar';
 import { BottomButtonContainer, Page, ScrollableContent } from '../../styles/layout';
