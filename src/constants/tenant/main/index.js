7ada6b8baff72e6ca225a34cc3a000eb9961e154
yminjuu <ymj0580@naver.com>
2025-10-20 23:01:11 +0900

    chore: 집주인 인증 전 텍스트 추가

--- src/constants/tenant/main/index.js
+++ src/constants/tenant/main/index.js
@@ -1,31 +1,35 @@
 export const AUTH_TEXT = {
-        'black' : [
-         "집주인이 이름 / 휴대폰 번호 / 주소 정보를 바탕으로 확인중이에요.",
-            "인증요청 후 집주인이 확인하면 인증이 완료됩니다. ",
-        ],
-        'primary' : [
-            "인증이 완료되면 알림을 보내드릴게요!",
-        ]
-    }
+  TENANT: {
+    black: [
+      '집주인이 이름 / 휴대폰 번호 / 주소 정보를 바탕으로 확인중이에요.',
+      '인증요청 후 집주인이 확인하면 인증이 완료됩니다. ',
+    ],
+    primary: ['인증이 완료되면 알림을 보내드릴게요!'],
+  },
+  LANDLORD: {
+    black: ['등기부 등본을 바탕으로 주소를 인증 중이에요'],
+    primary: ['인증이 완료되면 알림을 보내드릴게요!'],
+  },
+};
 
 export const NOTICE_CARD_MOCK = [
-    {
-      id: 1,
-      title: '분리수거 안내',
-      date: '2022-11-04', // 표시용은 YYYY.MM.DD로 포맷해서 쓰면 됨
-      pinned: true,       // 핀 고정
-      border: true,
-    },
-    {
-      id: 2,
-      title: '한양빌라 관리비 인하 공지',
-      date: '2024-12-01',
-      pinned: false,
-    },
-    {
-      id: 3,
-      title: '층간소음 관련 공지',
-      date: '2024-12-12',
-      pinned: false,
-    },
-  ];
+  {
+    id: 1,
+    title: '분리수거 안내',
+    date: '2022-11-04', // 표시용은 YYYY.MM.DD로 포맷해서 쓰면 됨
+    pinned: true, // 핀 고정
+    border: true,
+  },
+  {
+    id: 2,
+    title: '한양빌라 관리비 인하 공지',
+    date: '2024-12-01',
+    pinned: false,
+  },
+  {
+    id: 3,
+    title: '층간소음 관련 공지',
+    date: '2024-12-12',
+    pinned: false,
+  },
+];
