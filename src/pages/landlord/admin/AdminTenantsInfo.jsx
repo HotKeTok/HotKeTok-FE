00409c0b5276459aec2493558717d273491e8566
yminjuu <ymj0580@naver.com>
2025-10-26 04:14:55 +0900

    feat: 입주민 정보 임시 커밋

--- src/pages/landlord/admin/AdminTenantsInfo.jsx
+++ src/pages/landlord/admin/AdminTenantsInfo.jsx
@@ -1,12 +1,32 @@
 import AdminTenantsInfoTemplate from '../../../templates/landlord/admin/AdminTenantsInfoTemplate';
-import { useState } from 'react';
-import { EXAMPLE_ADMIN_TENANTS } from '../../../mocks/landlord/AdminTenants';
+import { useEffect, useState } from 'react';
+import { getTenantList } from '../../../api/post-service';
+import { useAuthStore } from '../../../store/useAuthStore';
 
 export default function AdminTenantsInfo() {
-  const [tenantsList, setTenantsList] = useState(EXAMPLE_ADMIN_TENANTS);
+  const accessToken = useAuthStore(state => state.accessToken);
+  const [loading, setLoading] = useState(false);
+  const [tenantsList, setTenantsList] = useState([]);
 
   // todo : 입주민 목록 조회 api
-  const getTenantsList = async () => {};
+  const getTenantsList = async () => {
+    try {
+      setLoading(true);
+      const response = await getTenantList(accessToken);
+      if (response.success) {
+        setTenantsList(response.data.floors);
+      }
+    } catch (error) {
+      console.error('입주민 목록 조회 실패:', error);
+      setTenantsList([]);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  return <AdminTenantsInfoTemplate tenantsList={tenantsList} />;
+  useEffect(() => {
+    getTenantsList();
+  }, [accessToken]);
+
+  return <AdminTenantsInfoTemplate tenantsList={tenantsList} loading={loading} />;
 }
