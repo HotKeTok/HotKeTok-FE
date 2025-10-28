f7bc8b6c547e3ec1e77d553717dd552086ce2142
sunwoo-h <flexible8011@gmail.com>
2025-10-28 16:02:54 +0900

    setting: vite.config.js 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr({ exportAsDefault: true })],
+  plugins: [
+    react(),
+    svgr({ exportAsDefault: true }), // default import로 받으려면 이 옵션이 편함
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets',
+    },
+  },
 });
