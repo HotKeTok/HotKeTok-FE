641a98177a5ac7edb4308e97c8c5b2f24a0c65cd
yminjuu <ymj0580@naver.com>
2025-10-29 09:06:24 +0900

    chore: 엔드포인트 변경

--- src/store/useChatStore.js
+++ src/store/useChatStore.js
@@ -142,7 +142,7 @@ const useChatStore = create((set, get) => ({
     const client = get().stompClient;
     if (client && get().isConnected) {
       client.publish({
-        destination: '/topic/chat/message',
+        destination: '/pub/chat/message',
         body: JSON.stringify({
           roomId,
           senderId,
